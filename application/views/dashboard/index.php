989e3f3784514d6edb5ca321a1a6175b5cc39b0d
ivan koop <ivankoop3@gmail.com>
2018-01-17 11:45:25 -0300

    first commit

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,1 @@
+<h1>HOLIS COMO ESTAN</h1>
---
+++ application/views/inc/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>CMS Structure</title>
+  </head>
+  <body>
+  <!--HEADER END HERE-->
