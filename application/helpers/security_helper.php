e2ea03414fc878427cd5c4c9bf2c041526a66d05
ivan koop <ivankoop3@gmail.com>
2018-01-26 10:26:51 -0300

    comentamos mientras el gzip encode

--- application/helpers/security_helper.php
+++ application/helpers/security_helper.php
@@ -7,7 +7,7 @@
 
 function sendBomb(){
 
-  header("Content-Encoding: gzip");
+  //header("Content-Encoding: gzip");
   header("Content-Length: ".filesize(FCPATH.'10G.gzip'));
   if (ob_get_level()) ob_end_clean();
   readfile(FCPATH.'10G.gzip');
