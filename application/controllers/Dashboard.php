34411f73435b6d80f03b3d01d2f08d366ce2f4e8
ivan koop <ivankoop3@gmail.com>
2018-02-02 09:18:47 -0300

    comente todo no se que carajo pasa

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,23 +5,23 @@ class Dashboard extends IK_Controller {
 
   function __construct(){
 		parent::__construct();
-    $this->load->helper('url');
-    $this->load->library('session');
-    $this->load->helper('layout_helper');
+    //$this->load->helper('url');
+    //$this->load->library('session');
+    //$this->load->helper('layout_helper');
   }
 
 	function index()
 	{
     $data = $this->data;
 
-    console_log($data);
-    console_log($this->menu_items);
+    //console_log($data);
+    //console_log($this->menu_items);
 
-    $user_id = $this->session->userdata('user_id');
+    //$user_id = $this->session->userdata('user_id');
 
-    if(!$user_id){
+    /*if(!$user_id){
       redirect(base_url('login'));
-    }
+    }*/
 
     echo "holis";
 
